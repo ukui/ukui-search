39a6382377fa426a5cf75944d6d27b0803d28685
zhangpengfei <zhangpengfei@kylinos.com>
2021-01-29 11:43:07 +0800

    Update copyright.

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -1,10 +1,11 @@
 /*
- * Copyright (C) 2019 Tianjin KYLIN Information Technology Co., Ltd.
  *
- * This program is free software; you can redistribute it and/or modify
+ * Copyright (C) 2020, KylinSoft Co., Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3, or (at your option)
- * any later version.
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,7 +13,10 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses/&gt;.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Authors: zhangjiaping <zhangjiaping@kylinos.cn>
+ * Modified by: zhangpengfei <zhangpengfei@kylinos.cn>
  *
  */
 
