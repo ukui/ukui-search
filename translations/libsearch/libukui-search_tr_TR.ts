7d8e20b25898622743aba263067ca3f19dbeba5c
Aylinux <cihanalk@gmail.com>
2021-02-04 20:39:15 +0300

    Create libukui-search_tr_TR.ts

---
+++ translations/libsearch/libukui-search_tr_TR.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr_TR">
+<context>
+    <name>GlobalSettings</name>
+    <message>
+        <location filename="../../libsearch/global-settings.cpp" line="89"/>
+        <source>I can&apos;t remove an empty path string!</source>
+        <translation>Boş bir yol dizesini kaldıramıyorum!</translation>
+    </message>
+    <message>
+        <location filename="../../libsearch/global-settings.cpp" line="98"/>
+        <source>I can only search your user directory, it doesn&apos;t make any sense if you block an directory which is not in user directory!</source>
+        <translation>Yalnızca kullanıcı dizininizi arayabilirim, kullanıcı dizininde olmayan bir dizini engellemeniz bir anlam ifade etmez!</translation>
+    </message>
+    <message>
+        <location filename="../../libsearch/global-settings.cpp" line="110"/>
+        <source>My parent folder has been blocked!</source>
+        <translation>Ana klasörüm engellendi!</translation>
+    </message>
+</context>
+</TS>
