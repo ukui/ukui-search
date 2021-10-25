b18d9301f1a7335392bdd6a8b8e4537589d67bed
iaom <zhangpengfei@kylinos.cn>
2021-10-25 14:06:17 +0800

    Fix:Search input box icon crooked when zoom to 150%.

--- src/input-box.cpp
+++ src/input-box.cpp
@@ -108,8 +108,8 @@ void SearchBarHLayout::initUI() {
     QPixmap pixmap(QIcon::fromTheme("system-search-symbolic", QIcon(":/res/icons/system-search.symbolic.png")).pixmap(QSize(20, 20)));
     m_queryIcon = new QLabel;
     m_queryIcon->setProperty("useIconHighlightEffect", 0x10);
-    m_queryIcon->setFixedSize(pixmap.size());
     m_queryIcon->setPixmap(pixmap);
+    m_queryIcon->adjustSize();
 
     m_queryText = new QLabel;
     m_queryText->setText(tr("Search"));
