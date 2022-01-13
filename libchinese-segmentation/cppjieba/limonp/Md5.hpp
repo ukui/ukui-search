8f16f2cf1cc55872e60de7fc59c89bb2c7f7da69
jixiaoxu <jixiaoxu@kylinos.cn>
2022-01-13 21:10:24 +0800

    更新copyright；

--- libchinese-segmentation/cppjieba/limonp/Md5.hpp
+++ libchinese-segmentation/cppjieba/limonp/Md5.hpp
@@ -1,33 +1,23 @@
-/****************************************************************************
-**Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991
-**              2020, KylinSoft Co., Ltd.
-**All rights reserved.
-**
-**License to copy and use this software is granted provided that it
-**is identified as the "RSA Data Security, Inc. MD5 Message-Digest
-**Algorithm" in all material mentioning or referencing this software
-**or this function.
-**
-**License is also granted to make and use derivative works provided
-**that such works are identified as "derived from the RSA Data
-**Security, Inc. MD5 Message-Digest Algorithm" in all material
-**mentioning or referencing the derived work.
-**
-**RSA Data Security, Inc. makes no representations concerning either
-**the merchantability of this software or the suitability of this
-**software for any particular purpose. It is provided "as is"
-**without express or implied warranty of any kind.
-**
-**These notices must be retained in any copies of any part of this
-**documentation and/or software.
-**
-**
-**
-**The original md5 implementation avoids external libraries.
-**This version has dependency on stdio.h for file input and
-**string.h for memcpy.
-**
-****************************************************************************/
+/*
+ * Copyright (C) 2022, KylinSoft Co., Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Authors: yanyiwu
+ * Modified by: jixiaoxu <jixiaoxu@kylinos.cn>
+ *
+ */
 
 #ifndef __MD5_H__
 #define __MD5_H__
