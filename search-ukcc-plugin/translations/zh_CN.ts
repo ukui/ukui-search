b8303f2efaa89bd6e99866d34c0abcac781e2458
baijunjie <baijunjie@kylinos.cn>
2022-01-28 09:34:22 +0800

    update the translation file.

--- search-ukcc-plugin/translations/zh_CN.ts
+++ search-ukcc-plugin/translations/zh_CN.ts
@@ -4,124 +4,124 @@
 <context>
     <name>Search</name>
     <message>
-        <location filename="../search.cpp" line="13"/>
-        <location filename="../search.cpp" line="130"/>
+        <location filename="../search.cpp" line="32"/>
+        <location filename="../search.cpp" line="148"/>
         <source>Search</source>
-        <translation>搜索</translation>
+        <translation>全局搜索</translation>
     </message>
     <message>
-        <location filename="../search.cpp" line="154"/>
+        <location filename="../search.cpp" line="172"/>
         <source>Create index</source>
         <translation>创建索引</translation>
         <extra-contents_path>/Search/Create index</extra-contents_path>
     </message>
     <message>
-        <location filename="../search.cpp" line="155"/>
+        <location filename="../search.cpp" line="173"/>
         <source>Creating index can help you getting results quickly.</source>
         <translation>开启之后可以快速获取搜索结果</translation>
     </message>
     <message>
-        <location filename="../search.cpp" line="174"/>
+        <location filename="../search.cpp" line="192"/>
         <source>Default web searching engine</source>
         <translation>默认互联网搜索引擎</translation>
         <extra-contents_path>/Search/Default web searching engine</extra-contents_path>
     </message>
     <message>
-        <location filename="../search.cpp" line="178"/>
+        <location filename="../search.cpp" line="196"/>
         <source>baidu</source>
         <translation>百度</translation>
     </message>
     <message>
-        <location filename="../search.cpp" line="179"/>
+        <location filename="../search.cpp" line="197"/>
         <source>sougou</source>
         <translation>搜狗</translation>
     </message>
     <message>
-        <location filename="../search.cpp" line="180"/>
+        <location filename="../search.cpp" line="198"/>
         <source>360</source>
         <translation>360</translation>
     </message>
     <message>
-        <location filename="../search.cpp" line="188"/>
+        <location filename="../search.cpp" line="206"/>
         <source>Block Folders</source>
         <translation>排除的文件夹</translation>
         <extra-contents_path>/Search/Block Folders</extra-contents_path>
     </message>
     <message>
-        <location filename="../search.cpp" line="193"/>
+        <location filename="../search.cpp" line="211"/>
         <source>Following folders will not be searched. You can set it by adding and removing folders.</source>
         <translation>搜索将不查看以下文件夹，通过添加和删除可以设置排除的文件夹位置</translation>
     </message>
     <message>
-        <location filename="../search.cpp" line="235"/>
+        <location filename="../search.cpp" line="251"/>
         <source>Choose folder</source>
         <translation>添加</translation>
     </message>
     <message>
-        <location filename="../search.cpp" line="348"/>
+        <location filename="../search.cpp" line="364"/>
         <source>delete</source>
         <translation>删除</translation>
     </message>
     <message>
-        <location filename="../search.cpp" line="403"/>
+        <location filename="../search.cpp" line="419"/>
         <source>Directories</source>
         <translation>文件夹</translation>
     </message>
     <message>
-        <location filename="../search.cpp" line="404"/>
+        <location filename="../search.cpp" line="420"/>
         <source>select blocked folder</source>
         <translation>选择排除的文件夹</translation>
     </message>
     <message>
-        <location filename="../search.cpp" line="405"/>
+        <location filename="../search.cpp" line="421"/>
         <source>Select</source>
         <translation>选择</translation>
     </message>
     <message>
-        <location filename="../search.cpp" line="406"/>
+        <location filename="../search.cpp" line="422"/>
         <source>Position: </source>
         <translation>位置</translation>
     </message>
     <message>
-        <location filename="../search.cpp" line="407"/>
+        <location filename="../search.cpp" line="423"/>
         <source>FileName: </source>
         <translation>文件名</translation>
     </message>
     <message>
-        <location filename="../search.cpp" line="408"/>
+        <location filename="../search.cpp" line="424"/>
         <source>FileType: </source>
         <translation>类型</translation>
     </message>
     <message>
-        <location filename="../search.cpp" line="409"/>
+        <location filename="../search.cpp" line="425"/>
         <source>Cancel</source>
         <translation>取消</translation>
     </message>
     <message>
-        <location filename="../search.cpp" line="425"/>
-        <location filename="../search.cpp" line="429"/>
-        <location filename="../search.cpp" line="433"/>
-        <location filename="../search.cpp" line="437"/>
+        <location filename="../search.cpp" line="441"/>
+        <location filename="../search.cpp" line="445"/>
+        <location filename="../search.cpp" line="449"/>
+        <location filename="../search.cpp" line="453"/>
         <source>Warning</source>
         <translation>警告</translation>
     </message>
     <message>
-        <location filename="../search.cpp" line="425"/>
+        <location filename="../search.cpp" line="441"/>
         <source>Add blocked folder failed, choosen path is empty!</source>
         <translation>添加失败，选择的路径为空！</translation>
     </message>
     <message>
-        <location filename="../search.cpp" line="429"/>
+        <location filename="../search.cpp" line="445"/>
         <source>Add blocked folder failed, it is not in home path!</source>
         <translation>添加失败，添加的路径不在家目录下！</translation>
     </message>
     <message>
-        <location filename="../search.cpp" line="433"/>
+        <location filename="../search.cpp" line="449"/>
         <source>Add blocked folder failed, its parent dir is exist!</source>
         <translation>添加失败，父目录已被添加！</translation>
     </message>
     <message>
-        <location filename="../search.cpp" line="437"/>
+        <location filename="../search.cpp" line="453"/>
         <source>Add blocked folder failed, it has been already blocked!</source>
         <translation>添加失败，这个文件夹已经被添加过了！</translation>
     </message>
